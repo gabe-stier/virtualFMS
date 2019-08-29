503890b99ef7751bf0db5771dcd9afa3fc2d0357
Gabe <gabezter4@yahoo.com>
2019-08-29 13:29:53 -0500

    Made file finding function.

--- src/virtualFMS/FileHandler.java
+++ src/virtualFMS/FileHandler.java
@@ -1,5 +1,21 @@
 package virtualFMS;
 
+import java.io.File;
+
 public class FileHandler {
+
+	public void listFiles(File folder) {
+		final File[] listOfFiles = folder.listFiles();
+		File[] array;
+		for (int length = (array = listOfFiles).length, i = 0; i < length; ++i) {
+			final File file = array[i];
+			if (file.isFile()) {
+				System.out.println("File - " + folder.getName() + " - " + file.getName());
+			} else {
+				System.out.println("Folder - " + folder.getName() + " - " + file.getName());
+				listFiles(file);
+			}
+		}
+	}
 
 }
